36f33945fbeebd27316906404ac1082a05e6f942
Freddy-byte2004 <freddyalvarado988@gmail.com>
2025-10-13 21:32:18 -0400

    cambios de link

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -63,7 +63,7 @@ return(
         </div>
         <div className='pie'>
           <div className='Olvido-contrasena'><a href='#'>¿Has olvidado la contraseña?</a></div>  
-            <div className='registro'><a href='https://diabetes-ia-1.onrender.com/registro'>Regístrate</a></div>
+            <div className='registro'><a href='https://diabetes-ia-1.onrender.com/#/registro'>Regístrate</a></div>
         </div>
     </div>
   </div>
