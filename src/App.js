14ba22c350e69c01cabfbfc60275fe46a7d1a7fc
Freddy-byte2004 <freddyalvarado988@gmail.com>
2025-10-13 21:19:01 -0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from  './Pages/Login';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
 import UsuarioPrincipal from './Pages/UsuarioPrincipal';
 import RegistroUsuario from './Pages/RegistroUsuario';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<UsuarioPrincipal />} />
-      <Route path="/registro" element={<RegistroUsuario />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<UsuarioPrincipal />} />
+        <Route path="/registro" element={<RegistroUsuario />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
