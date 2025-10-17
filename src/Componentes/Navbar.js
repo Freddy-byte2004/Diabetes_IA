0d656aa9ee04db327e8efb0d336ea45a322da7c5
Freddy-byte2004 <freddyalvarado988@gmail.com>
2025-10-17 15:13:45 -0400

    A

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
               <div className="Inicio"><a href='#'> <div className="icono-navbar"><AiFillHome /></div><div className="texto-navbar">Inicio</div></a></div>
               <div className="Historial"><Link to='#'><div className="icono-navbar"><AiFillClockCircle/></div><div className="texto-navbar">Historial</div></Link></div>
               <div className="Perfil"><Link to='#'><div className="icono-navbar"><AiFillProfile/></div><div className="texto-navbar">Perfil</div></Link></div>
-              <div className="CerrarSesion"><Link to='https://diabetes-ia-1.onrender.com/'><div className="icono-navbar"><AiFillCloseCircle/></div><div className="texto-navbar">Cerrar Sesión</div></Link></div>
+              <div className="CerrarSesion" onClick={cerrarSesion}><Link to='https://diabetes-ia-1.onrender.com/'><div className="icono-navbar"><AiFillCloseCircle/></div><div className="texto-navbar">Cerrar Sesión</div></Link></div>
             </div>
         </nav>
     )
